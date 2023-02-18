6fd44abc4a50be5b2816df1cbbde51c17a1f1015
Reid-T-W <rediettadesse100@gmail.com>
2023-02-18 08:25:21 +0300

    regex removed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,42 +6,24 @@ import re
 
 status_code_dict = {200: 0, 301: 0, 400: 0, 401: 0,
                     403: 0, 404: 0, 405: 0, 500: 0}
-# compiling the correct pattern
-pat = re.compile(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} - "
-                 r"\[[0-9]{4}-[0-9]{2}-[0-9]{1,2} [0-9]{2}:[0-9]{2}:"
-                 r"[0-9]{2}.[0-9]{6}\] "
-                 r"\"GET \/projects\/260 HTTP\/1.1\" [0-9]{3} [0-9]*")
 
-pat_no_status = re.compile(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} - "
-                           r"\[[0-9]{4}-[0-9]{2}-[0-9]{1,2} [0-9]{2}:[0-9]{2}:"
-                           r"[0-9]{2}.[0-9]{6}\] "
-                           r"\"GET \/projects\/260 HTTP\/1.1\" [0-9]*")
 total_size = 0
 counter = 0
 
 try:
     for line in sys.stdin:
-        # Check input format of line
-        if re.match(pat, line):
-            # Extract file size
-            params = line.split()
-            file_size = int(params[-1])
-            status = int(params[-2])
-            counter = counter + 1
-        elif re.match(pat_no_status, line):
-            # Extract file size
-            params = line.split()
-            file_size = int(params[-1])
-            counter = counter + 1
-        else:
-            counter = counter + 1
+        tokens = line.split()
+        if len(tokens) != 9:
             continue
-
-        total_size = total_size + file_size
         try:
+            file_size = int(tokens[-1])
+            status = int(tokens[-2])
+            counter = counter + 1
+            total_size = total_size + file_size
             status_code_dict[status] = status_code_dict[status] + 1
         except Exception:
             pass
+
         if (counter % 10 == 0):
             print("File size: {}".format(total_size))
             try:
@@ -63,7 +45,10 @@ finally:
     # remaining 9, the code below prints out the summary of the 9 logs.
     if (counter % 10 != 0):
         print("File size: {}".format(total_size))
-        status_code_dict_sorted = sorted(status_code_dict.items())
-        for code, count in dict(status_code_dict_sorted).items():
-            if count != 0:
-                print("{}: {}".format(code, count))
+        try:
+            status_code_dict_sorted = sorted(status_code_dict.items())
+            for code, count in dict(status_code_dict_sorted).items():
+                if count != 0:
+                    print("{}: {}".format(code, count))
+        except Exception:
+            pass
