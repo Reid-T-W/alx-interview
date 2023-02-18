3d349a1d6a86f94eda4f0a8a914cfdb6c492532f
Reid-T-W <rediettadesse100@gmail.com>
2023-02-19 00:05:40 +0300

    script cannot be executed when imported

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,34 +10,35 @@ status_code_dict = {200: 0, 301: 0, 400: 0, 401: 0,
 total_size = 0
 counter = 0
 
-try:
-    for line in sys.stdin:
-        # print(line)
-        tokens = line.split()
-        if len(tokens) < 2:
-            continue
-        file_size = int(tokens[-1])
-        status = int(tokens[-2])
-        counter = counter + 1
-        total_size = total_size + file_size
-        status_code_dict[status] = status_code_dict[status] + 1
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            # print(line)
+            tokens = line.split()
+            if len(tokens) < 2:
+                continue
+            file_size = int(tokens[-1])
+            status = int(tokens[-2])
+            counter = counter + 1
+            total_size = total_size + file_size
+            status_code_dict[status] = status_code_dict[status] + 1
 
-        if (counter == 10):
+            if (counter == 10):
+                print("File size: {}".format(total_size))
+                status_code_dict_sorted = sorted(status_code_dict.items())
+                for code, count in dict(status_code_dict_sorted).items():
+                    if count != 0:
+                        print("{}: {}".format(code, count))
+                counter = 0
+    finally:
+        # This is required in cases were the total number of logs is not
+        # divisible by 10
+        # Example if their are 19 logs, the first ten will be handled with
+        # the above code, but the loop will exit without processing the
+        # remaining 9, the code below prints out the summary of the 9 logs.
+        if counter != 0:
             print("File size: {}".format(total_size))
             status_code_dict_sorted = sorted(status_code_dict.items())
             for code, count in dict(status_code_dict_sorted).items():
                 if count != 0:
                     print("{}: {}".format(code, count))
-            counter = 0
-finally:
-    # This is required in cases were the total number of logs is not
-    # divisible by 10
-    # Example if their are 19 logs, the first ten will be handled with
-    # the above code, but the loop will exit without processing the
-    # remaining 9, the code below prints out the summary of the 9 logs.
-    if counter != 0:
-        print("File size: {}".format(total_size))
-        status_code_dict_sorted = sorted(status_code_dict.items())
-        for code, count in dict(status_code_dict_sorted).items():
-            if count != 0:
-                print("{}: {}".format(code, count))
