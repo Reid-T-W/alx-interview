a58630f5e7acee524d75636786cbd3703c19616e
Reid-T-W <rediettadesse100@gmail.com>
2023-02-20 12:59:27 +0300

    Wrong format handled correctly

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -18,20 +18,32 @@ if __name__ == "__main__":
             if len(tokens) < 2:
                 continue
             file_size = int(tokens[-1])
-            status = int(tokens[-2])
+            # Try and except clause are added for all operations related
+            # to status, this is done in order to handle wrong format
+            try:
+                status = int(tokens[-2])
+            except Exception:
+                pass
             counter = counter + 1
             total_size = total_size + file_size
-            if status in status_code_dict:
-                status_code_dict[status] += 1
+            try:
+                if status in status_code_dict:
+                    status_code_dict[status] += 1
+            except Exception:
+                pass
 
             if (counter == 10):
                 print("File size: {}".format(total_size))
                 # status_code_dict_sorted = sorted(status_code_dict.items())
-                status_code_sorted = sorted(status_code_dict.items())
-                status_code_dict_sorted = {k: v for k, v in status_code_sorted}
-                for code, count in dict(status_code_dict_sorted).items():
-                    if count != 0:
-                        print("{}: {}".format(code, count))
+                try:
+                    status_code_sorted = sorted(status_code_dict.items())
+                    status_code_dict_sorted = {k: v for k, v
+                                               in status_code_sorted}
+                    for code, count in dict(status_code_dict_sorted).items():
+                        if count != 0:
+                            print("{}: {}".format(code, count))
+                except Exception:
+                    pass
                 counter = 0
     finally:
         # This is required in cases were the total number of logs is not
@@ -41,8 +53,11 @@ if __name__ == "__main__":
         # remaining 9, the code below prints out the summary of the 9 logs.
         if counter != 0:
             print("File size: {}".format(total_size))
-            status_code_sorted = sorted(status_code_dict.items())
-            status_code_dict_sorted = {k: v for k, v in status_code_sorted}
-            for code, count in dict(status_code_dict_sorted).items():
-                if count != 0:
-                    print("{}: {}".format(code, count))
+            try:
+                status_code_sorted = sorted(status_code_dict.items())
+                status_code_dict_sorted = {k: v for k, v in status_code_sorted}
+                for code, count in dict(status_code_dict_sorted).items():
+                    if count != 0:
+                        print("{}: {}".format(code, count))
+            except Exception:
+                pass
