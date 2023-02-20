2f853b9a78e3b0e4539aec68e3e6098fc1e4617c
Reid-T-W <rediettadesse100@gmail.com>
2023-02-20 09:45:01 +0000

    sorting corrected to work for finally block

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -41,7 +41,8 @@ if __name__ == "__main__":
         # remaining 9, the code below prints out the summary of the 9 logs.
         if counter != 0:
             print("File size: {}".format(total_size))
-            status_code_dict_sorted = sorted(status_code_dict.items())
+            status_code_sorted = sorted(status_code_dict.items())
+            status_code_dict_sorted = {k: v for k, v in status_code_sorted}
             for code, count in dict(status_code_dict_sorted).items():
                 if count != 0:
                     print("{}: {}".format(code, count))
