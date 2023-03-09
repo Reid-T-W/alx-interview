26acf4f81ca442af66b47b345a0281edc2e37d39
Reid-T-W <rediettadesse100@gmail.com>
2023-03-09 08:41:09 +0300

    shebang changed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const request = require('request');
 
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
@@ -31,4 +31,3 @@ async function getData (url) {
   }
 }
 getData(url);
-
