7d93ea833c1bcbf5a92aa228115e21d57ad2cdbd
Reid-T-W <rediettadesse100@gmail.com>
2023-03-25 10:40:21 +0300

    empty coins handled

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -73,6 +73,9 @@ def makeChange(coins, total):
     num_of_coins = 0
     if total <= 0:
         return 0
+    # checking for empty coins
+    if not coins:
+        return -1
     coins = sorted(coins)
     first_big = len(coins) - 1
     # Checking if the mimimum coin we have is
@@ -80,11 +83,6 @@ def makeChange(coins, total):
     # In that case total cannot be achieved
     if (coins[0] > total):
         return -1
-    # remainder = total % coins[first_big]
-    # remaining = total // coins[first_big]
-    # if remaining == 0:
-    #     num_of_coins = remainder
-    #     return num_of_coins
     adj_total = total
     while ((adj_total) != 0 and first_big >= 0):
         if coins[first_big] > adj_total:
@@ -93,6 +91,9 @@ def makeChange(coins, total):
             num_of_coins += (adj_total // coins[first_big])
             adj_total = adj_total % coins[first_big]
             first_big -= 1
+    # This check is required because the loop might have
+    # exited due to first_big being less than 0 (meaning
+    # its impossible to achieve total with the coins we have)
     if (adj_total == 0):
         return num_of_coins
     return -1
